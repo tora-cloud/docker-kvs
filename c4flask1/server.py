0d5906ca5239c19e8eb57e7cb1d6600dea38900f
tora-cloud <masatora_torii@yahoo.co.jp>
2026-01-12 10:05:35 +0900

    4th commit

--- c4flask1/server.py
+++ c4flask1/server.py
@@ -3,5 +3,5 @@ PORT = int(os.environ['PORT'])
 app = flask.Flask('app server')
 @app.route('/')
 def index():
-  return 'hello Dockerfile3'
+  return 'hello Dockerfile4'
 app.run(debug=False, host='0.0.0.0', port=PORT)
